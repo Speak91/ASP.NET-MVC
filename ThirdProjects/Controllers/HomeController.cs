c6a3c2d6c52e23cb79c7b94411e28ca9d250c8af
Valentin Illarionov <i-valentin@mail.ru>
2022-02-14 22:58:36 +0900

    Выполнено 3 задание
    Создайте веб приложение. Создайте модель
    Product, в которую входят поля ID, Name, Price, CreatedDate.
    Создайте контроллер и метод действия, который создает коллекцию из нескольких элементов
    и передает эту коллекцию в качестве модели представлению.
    Представление, получившее данные от контроллера, должно отобразить эти данные в
    нескольких вариантах:
    1. В виде списка со следующими данными: ID. Name – Price$ (CreatedDate)
    2. В виде таблицы с колонками Id, Name, Prcice, Created Date
    Обязательное условие при выводе продуктов использовать частичные представления.

---
+++ ThirdProjects/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using ThirdProjects.Models;
+
+namespace ThirdProjects.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            List<Product> products = new List<Product>()
+            {
+                new Product(){Id= 1, Name = "Product #1", Price = 2000, CreateTime= new DateTime(2020,11,06)},
+                new Product(){Id= 2, Name = "Product #2", Price = 2100, CreateTime= new DateTime(2020,10,05)},
+                new Product(){Id= 3, Name = "Product #3", Price = 2200, CreateTime= new DateTime(2020,05,24)},
+                new Product(){Id= 4, Name = "Product #4", Price = 2300, CreateTime= new DateTime(2020,08,15)},
+                new Product(){Id= 5, Name = "Product #5", Price = 2400, CreateTime= new DateTime(2020,01,19)}
+            };
+            return View(products);
+        }
+    }
+}
---
+++ ThirdProjects/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ThirdProjects.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public DateTime CreateTime { get; set; }
+    }
+}
