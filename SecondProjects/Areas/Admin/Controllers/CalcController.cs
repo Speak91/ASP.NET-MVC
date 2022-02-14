78d2f8555bd7ffa4b089565912ad9ddfbdf8b551
Valentin Illarionov <i-valentin@mail.ru>
2022-02-14 22:00:51 +0900

    Выполнено 2 задание
    Создайте веб приложение содержащее область для администратора с именем Admin.
    Создайте несколько представлений и контроллеров. Для основной части приложения создайте
    контроллер Home с методами действия Index, About, Contacts сделайте так, чтобы все
    представления, возвращаемые данными методами содержали ссылки на другие методы
    действий контроллера Home. В области Admin создайте контроллер, который будет обслуживать следующие запросы выполняя
    арифметические операции –
    Calc/Add/10/20 в ответ возвращается страница с текстом 30
    Calc/Mul/5/5 в ответ возвращается страница с текстом 25
    Calc/Sub/10/3 в ответ возвращается страница с текстом 7
    Calc/Div/10/2 в ответ возвращается страница с текстом 5

---
+++ SecondProjects/Areas/Admin/Controllers/CalcController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SecondProjects.Areas.Admin.Controllers
+{
+    public class CalcController : Controller
+    {
+        // GET: Admin/Calc
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Add(int? x, int? y)
+        {
+            ViewBag.Result = x + y;
+            return View("Index");
+        }
+
+        public ActionResult Mul(int? x, int? y)
+        {
+            ViewBag.Result = x * y;
+            return View("Index");
+        }
+
+        public ActionResult Sub(int? x, int? y)
+        {
+            ViewBag.Result = x - y;
+            return View("Index");
+        }
+
+        public ActionResult Div(double? x, double? y)
+        {
+            ViewBag.Result = x / y;
+            return View("Index");
+        }
+    }
+}
